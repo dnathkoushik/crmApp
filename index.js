3fa283b27ea44bad5599e4d25d161c670d48eaa4
Koushik Debnath <koushikcomhere@gmail.com>
2026-01-13 20:55:17 +0530

    Connected with mongo database

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+
+//Start a server at the specified PORT
+const PORT = process.env.PORT || 7777;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
+
+//Code to make a connection with mongoDB
+const mongoose = require('mongoose');   
+
+async function connectDB() {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+}
+
+connectDB();
+
+//Middleware to parse JSON requests
+app.use(express.json());
