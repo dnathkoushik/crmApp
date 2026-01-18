83bc23e967a170c94a691d8254a9290a7330897b
Koushik Debnath <koushikcomhere@gmail.com>
2026-01-18 19:07:16 +0530

    Change User Status API Created

--- middleWares/auth.mw.js
+++ middleWares/auth.mw.js
@@ -1,5 +1,7 @@
 const userModel = require('../models/user.model');
 const constants = require('../utils/constants');
+const jwt = require('jsonwebtoken');
+const config = require("./../configs/auth.config");
 
 exports.validateSignUpRequestBody = async (req, res, next) => {
     if(!req.body.name){
@@ -49,3 +51,32 @@ exports.validateSignInRequestBody = async (req, res, next) => {
     next();
 };
 
+exports.validateToken = (req, res, next) => {
+    let token = req.headers["x-access-token"];
+
+    if (!token) {
+        return res.status(403).send({
+            message: "No token provided!"
+        });
+    }
+    jwt.verify(token, config.secret, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({
+                message: "Unauthorized!"
+            });
+        }
+        req.userId = decoded.id;
+        next();
+    });
+};
+
+exports.isAdmin = async (req, res, next) => {
+    const user = await userModel.findOne({ userId: req.userId });
+    if (user.userType === constants.USER_TYPES.ADMIN) {
+        next();
+        return;
+    }
+    res.status(403).send({
+        message: "Require Admin Role!"
+    });
+};
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -6,5 +6,5 @@ const authMw = require('../middleWares/auth.mw');
 //SignUp Route
 route.post('/auth/signup', authMw.validateSignUpRequestBody, authController.signUp);
 route.post('/auth/signin', authMw.validateSignInRequestBody, authController.signIn);
-
+route.put('/auth/changeStatus/:userId', [authMw.validateToken, authMw.isAdmin], authController.changeUserStatus);
 module.exports = route;
