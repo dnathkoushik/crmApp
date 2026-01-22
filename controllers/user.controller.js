c7dc272496e9b61fe044506d7ba5bdc5d3896f34
Koushik Debnath <koushikcomhere@gmail.com>
2026-01-22 20:13:25 +0530

    Some changes and constrains are added to userType and userStatus

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,7 @@
 //This is the controller for user-related APIs
 const User = require('../models/user.model');
 const objectConverter = require('../utils/objectConverter');
+const constants = require('../utils/constants');
 
 //Controller to fetch all the users details
 exports.findAllUsers = async (req, res) => {
@@ -42,6 +43,9 @@ exports.findUserById = async (req, res) => {
 exports.changeUserStatus = async (req, res) => {
     const userId = req.params.userId;
     const newStatus = req.body.userStatus;
+    if(newStatus !== constants.USER_STATUS.APPROVED && newStatus !== constants.USER_STATUS.PENDING && newStatus !== constants.USER_STATUS.REJECTED){
+        return  res.status(400).send({ message: "Invalid user status value" });
+    }
     try {
         const user = await User.findOne({ userId: userId });
         if (!user) {
@@ -60,6 +64,9 @@ exports.changeUserStatus = async (req, res) => {
 exports.changeUserType = async (req, res) => {
     const userId = req.params.userId;
     const newType = req.body.userType;
+    if(newType !== constants.USER_TYPES.CUSTOMER && newType !== constants.USER_TYPES.ENGINEER && newType !== constants.USER_TYPES.ADMIN){
+        return  res.status(400).send({ message: "Invalid user type value" });
+    }
     try {
         const user = await User.findOne({ userId: userId });
         if (!user) {
