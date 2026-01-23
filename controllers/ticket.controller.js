fd99064397714064ed3826911582a566e538267b
Koushik Debnath <koushikcomhere@gmail.com>
2026-01-24 03:02:59 +0530

    ticket controller created to handel all ticket releted operations

---
+++ controllers/ticket.controller.js
@@ -0,0 +1,31 @@
+//Define the controller to create a new ticket
+//As soon as a ticket is created, it should be auto assigned to an engineer
+const ticketModel = require('../models/ticket.model');
+const userModel = require('../models/user.model');
+const constants = require('../utils/constants');
+
+ecports.createTicket = async (req, res) => {
+    const ticketObj = {
+        title: req.body.title,
+        ticketPriority: req.body.ticketPriority,
+        description: req.body.description,
+        status: req.body.status,
+        reporter: req.userId, //this userId will be set at the MW layer, during authentication
+    };
+
+    //Auto assign the ticket to an engineer
+    //I have to find an engineer whivh is in APPROVED state
+    const engineer = await userModel.findOne({ userType: constants.USER_TYPES.ENGINEER, userStatus: constants.USER_STATUS.APPROVED });
+    if(engineer){
+        ticketObj.assignee = engineer.userId;
+    }
+    try{
+        const newTicket = await ticketModel.create(ticketObj);
+        res.status(201).send(newTicket);
+    } catch(err){
+        console.log('Error while creating ticket', err);
+        res.status(500).send({
+            message: 'Internal Server Error while creating ticket'
+        });
+    }
+};
