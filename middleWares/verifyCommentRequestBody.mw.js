d256f9331f92eb652a6c82b8e1b71e4f4fe838d3
Koushik Debnath <koushikcomhere@gmail.com>
2026-01-27 00:42:17 +0530

    MiddleWare for validating comments created

---
+++ middleWares/verifyCommentRequestBody.mw.js
@@ -0,0 +1,19 @@
+const Ticket = require('../models/ticket.model');
+
+const validateCommentRequestBody = (req, res, next) => {
+    if (!req.body.commentText || req.body.commentText.trim() === '') {
+        return res.status(400).send({ message: 'Comment text is required' });
+    }
+    if(!req.params.ticketId || req.params.ticketId.trim() === ''){
+        return res.status(400).send({ message: 'Ticket ID is required in the URL parameters' });
+    }
+
+    const ticket = Ticket.findById(req.params.ticketId);
+    if (!ticket) {
+        return res.status(400).send({ message: 'Invalid Ticket ID' });
+    }
+
+    next();
+}
+
+module.exports = validateCommentRequestBody;
