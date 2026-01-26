5d7f1b2a40b37dca31172b4c1e8ff27dc84d6a22
Koushik Debnath <koushikcomhere@gmail.com>
2026-01-27 00:38:32 +0530

    Controller for Comment Created

---
+++ controllers/comment.controller.js
@@ -0,0 +1,35 @@
+//Controller to create a comment on a ticket
+const commentModel = require('../models/comment.model');
+const ticketModel = require('../models/ticket.model');
+const userModel = require('../models/user.model');
+const constants = require('../utils/constants');
+
+exports.createComment = async (req, res) => {
+    const commentObj = {
+        ticketId: req.params.ticketId,
+        commenter: req.userId, //this userId will be set at the MW layer, during authentication
+        commentText: req.body.commentText
+    };
+    //Check if ticket exists
+    const ticket = await ticketModel.findOne({ _id: commentObj.ticketId });
+    if(!ticket){
+        return res.status(400).send({ message: "Failed! Ticket does not exist" });
+    }
+    //Check if the user is either reporter, assignee or admin
+    const savedUser = await userModel.findOne({ userId: req.userId });
+    if(ticket.reporter !== req.userId && ticket.assignee !== req.userId && savedUser.userType !== constants.USER_TYPES.ADMIN){
+        return res.status(403).send({
+            message: 'Forbidden: You cannot comment on this ticket'
+        });
+    }
+    try{
+        const newComment = await commentModel.create(commentObj);
+        res.status(201).send(newComment);
+    }
+    catch(err){
+        console.log('Error while creating comment', err);
+        res.status(500).send({
+            message: 'Internal Server Error while creating comment'
+        });
+    }
+};
